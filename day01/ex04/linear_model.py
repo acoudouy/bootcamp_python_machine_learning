da0f33c6b61c3aabac0fe9d7c295d55661edcfae
acoudouy <acoudouy@student.42.fr>
2020-01-21 16:14:25 +0100

    day01ex04 done

--- day01/ex04/linear_model.py
+++ day01/ex04/linear_model.py
@@ -1,2 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as mlp
+import pandas as pd
+#import matplotlib.pyplot as mlp
+from mylinearregression import MyLinearRegression as MyLR
+#from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv("../resources/are_blue_pills_magics.csv")
+Xpill = np.array(data['Micrograms']).reshape(-1,1)
+Yscore = np.array(data['Score']).reshape(-1,1)
+
+linear_model1 = MyLR(np.array([[89.0], [-8]]))
+linear_model2 = MyLR(np.array([[89.0], [-6]]))
+Y_model1 = linear_model1.predict_(Xpill)
+Y_model2 = linear_model2.predict_(Xpill)
+
+
+print(linear_model1.mse_(Yscore, Y_model1))
+#print(mean_squared_error(Yscore, Y_model1))
+print(linear_model2.mse_(Yscore, Y_model2))
+#print(mean_squared_error(Yscore, Y_model1))
---
+++ day01/ex05/multi_linear_model.py
@@ -0,0 +1,6 @@
+import numpy as np
+import panda as pd
+from mylinearregression import MyLinearRegression as MyLR
+
+data = pd.read_csv("../resources/spacecraft_data.csv")
+
